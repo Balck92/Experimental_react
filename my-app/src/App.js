5aecb22bc7b4d4b0f9a4cbfd57592ecbeb3483d1
Carl Johan <carljohanbalck@gmail.com>
2020-03-04 21:21:53 +0100

    feat: added ew string again

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          Learn react today!
         </a>
       </header>
     </div>
