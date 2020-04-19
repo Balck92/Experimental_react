b9d835d8fab09c2ce2a837b11fc2fa6ad6588bad
Carl Johan <carljohanbalck@gmail.com>
2020-04-19 19:17:59 +0200

    Added more stuff

---
+++ my-app/src/components/CardPanel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from '@material-ui/core/Card';
+
+const CardPanel = ({ onIncrementAsync, onDecrementAsync, value }) => (
+    <div>
+        <Card></Card>
+
+    </div>
+);
+export default CardPanel;
--- my-app/src/components/CustomButton.js
+++ my-app/src/components/CustomButton.js
@@ -3,10 +3,10 @@ import React from "react";
 import Button from '@material-ui/core/Button';
 
 const CustomButton = ({ onIncrementAsync, onDecrementAsync, value }) => (
-    <div>
+    <div className="CustomButton">
         <Button variant="contained" color="primary" onClick={onIncrementAsync}>INCREASE</Button>
         <Button variant="contained" color="primary" onClick={onDecrementAsync}>DECREASE</Button>
-        <div>Clicked: {value} times</div>
+        <div className="ButtonText">Clicked: {value} times</div>
     </div>
 );
 export default CustomButton;
